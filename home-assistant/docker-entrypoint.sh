3f6270ec6892544a173beacec8ce23cb3b98f3ff
Daniel Carvalho Dias <danielcdias@users.noreply.github.com>
2025-09-19 23:08:29 -0300

    Alteração instalação HACS
    Alteração no script de entrypoint para adequar instalação do HACS no Home Assistant.

--- home-assistant/docker-entrypoint.sh
+++ home-assistant/docker-entrypoint.sh
@@ -6,25 +6,14 @@ CONFIG_DIR="/config"
 HACS_DIR="${CONFIG_DIR}/custom_components/hacs"
 
 # Function to install HACS more robustly
+# https://hacs.xyz/docs/use/download/download/#to-download-hacs-container
 install_hacs() {
   echo "HACS not found. Starting installation..."
 
-  # Navigate to the custom components directory
-  mkdir -p "${HACS_DIR}"
-  cd "${HACS_DIR}"
+  # mkdir -p "${HACS_DIR}"
+  cd "${CONFIG_DIR}"
 
-  # Download the zip file of the latest HACS version
-  # Using a fixed URL for the zip, which is more reliable than pip
-  wget -q -O hacs.zip "https://github.com/hacs/integration/archive/main.zip"
-
-  # Unzip the contents of the file
-  unzip -q hacs.zip
-
-  # Move the files to the correct directory
-  mv integration-main/* .
-
-  # Clean up temporary files
-  rm -rf integration-main hacs.zip
+  wget -O - https://get.hacs.xyz | bash -
 
   # Navigate back to the configuration directory
   cd "${CONFIG_DIR}"
@@ -43,21 +32,8 @@ else
     echo "HACS is already installed. No action needed."
 fi
 
-# Create the initial configuration file, if it doesn't exist
-if [ ! -f "${CONFIG_DIR}/configuration.yaml" ]; then
-    echo "Creating the initial configuration file..."
-    cat <<EOF > "${CONFIG_DIR}/configuration.yaml"
-# Reverse proxy configuration for Nginx
-http:
-  server_host:
-    - 0.0.0.0
-  use_x_forwarded_for: true
-  trusted_proxies:
-    - 0.0.0.0/0
-  login_attempts_threshold: 3
-EOF
-fi
-
 # Start the main Home Assistant service
 echo "Starting Home Assistant..."
-exec python3 -m homeassistant --config "${CONFIG_DIR}" "$@"
+# exec python3 -m homeassistant --config "${CONFIG_DIR}" "$@"
+exec /init
+
